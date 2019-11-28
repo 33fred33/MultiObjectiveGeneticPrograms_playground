d3a99dbdbde9027402f9ea6dd146aa3e201dbfb3
33fred33 <fredvaldezameneyro@gmail.com>
2019-11-28 18:37:44 +0000

    More complete version

--- GeneticProgram.py
+++ GeneticProgram.py
@@ -7,9 +7,10 @@ Created on Thu Nov 28 10:52:52 2019
 """
 import math
 import random as rd
+import time
 
 #Global variables
-logs_level = 0
+logs_level = 1
 
 class IndividualClass:
     def __init__(self, fenotype):
@@ -17,21 +18,36 @@ class IndividualClass:
         self.evaluation = None
         
     def __lt__(self, other): #less than
-        for eval_ind in range(len(self.evaluation)):
-            if self.evaluation[eval_ind] > other.evaluation[eval_ind]:
-                return False
-        return True
+        """
+        Each evaluation in this individual is used as a tiebreak for the previous one
+        """ #hola
+        if isinstance(self.evaluation, list):
+            for eval_ind in range(len(self.evaluation)):
+                if self.evaluation[eval_ind] > other.evaluation[eval_ind]:
+                    return False
+                if self.evaluation[eval_ind] < other.evaluation[eval_ind]:
+                    return True
+        else:
+            return self.evaluation < other.evaluation
+    
+    def __eq__(self, other):
+        if isinstance(self.evaluation, list):
+            for eval_ind in range(len(self.evaluation)):
+                if self.evaluation[eval_ind] != other.evaluation[eval_ind]:
+                    return False
+            return True
+        else:
+            return self.evaluation == other.evaluation
     
     def __str__(self):
         return str(self.fenotype)
 
-class GeneticProgram:
+class GeneticProgramClass:
     def __init__(
             self,
             population_size,
             generations,
             Model,
-            multi_objective = False, 
             sampling_method="tournament",
             mutation_ratio=0.02,
             tournament_size=2,
@@ -41,8 +57,9 @@ class GeneticProgram:
         population_size
         generations
         Model: class containing the methods:
-            generate_individual(): initialises a random individual
-        multi_objective
+            generate_individual(n): initialises n random individuals
+            mutate(individual): returns the mutated the individual
+            crossover(individual1, individual2): returns the crossover offspring individual from the given individuals
         sampling_method: can be tournament / weighted_random / random
         mutation_ratio is the ratio of the next generation non-elite population to be filled with mutation-generated individuals
         tournament_size only used if sampling method is tournament. Best out from randomly selected individuals will be selected for sampling
@@ -53,7 +70,6 @@ class GeneticProgram:
         self.population_size = population_size
         self.generations = generations
         self.Model = Model
-        self.multi_objective = multi_objective
         self.sampling_method = sampling_method
         self.mutation_ratio = mutation_ratio
         self.tournament_size = tournament_size
@@ -61,22 +77,32 @@ class GeneticProgram:
         
         #General variables initialisation
         self.darwin_champion = None
+        self.population = []
         
     def fit(self):
         """
-        
+        Executes the genetic program
             
         """
         
         #Initial population initialisation
-        self.population = [self.Model.generate_individual(self.population_size)]
+        self.population = [IndividualClass(individual) for individual in self.Model.generate_population(self.population_size)]
         self._evaluate_population()
         
         #amounts of each population type and procedence     
         mutations = math.ceil(self.population_size * self.mutation_ratio)
         crossovers = self.population_size - mutations
+        
+        if logs_level >= 1:
+            print("population_size: ", self.population_size)
+            print("mutations per gen: ", mutations)
+            print("crossovers per gen: ", crossovers)
             
         for generation in range(self.generations):
+            
+            if logs_level >= 1:
+                print("Generation: ", generation)
+                start_time = time.time()
                 
             #Parents selection
             if self.sampling_method == "tournament":    
@@ -86,7 +112,11 @@ class GeneticProgram:
             elif self.sampling_method == "weighted_random":
                 selected_first_parents = self._weighted_random_sample(self.population, crossovers)
                 selected_second_parents = self._weighted_random_sample(self.population, crossovers)
-                selected_mutations = self._weighted_random_sample(self.population, mutations)         
+                selected_mutations = self._weighted_random_sample(self.population, mutations)
+            else:
+                selected_first_parents = rd.choices(self.population, k = crossovers)
+                selected_second_parents = rd.choices(self.population,  k = crossovers)
+                selected_mutations = rd.choices(self.population,  k = mutations)
             
             #increase population
             self.population.extend([IndividualClass(self.Model.mutate(individual.fenotype)) for individual in selected_mutations])
@@ -97,6 +127,10 @@ class GeneticProgram:
             
             #select next generation's population
             self.population = sorted(self.population)[:self.population_size]
+            
+            if logs_level >= 1: 
+                print("Generation time: ", str(time.time() - start_time))
+                #print("Best individual so far: ", self.population[0].fenotype)
         
         #final individual selection
         self.darwin_champion = self.population[0]
@@ -104,20 +138,20 @@ class GeneticProgram:
         return self.darwin_champion
     
     def _evaluate_population(self):
-        for individual in self.population:
-            if individual.evaluation is None:
-                individual.evaluation = self.Model.evaluate(individual.fenotype)
+        individuals_fitness = self.Model.evaluate([individual.fenotype for individual in self.population])
+        for i,individual in enumerate(self.population):
+            individual.evaluation = individuals_fitness[i]
     
     def _weighted_random_sample(self, parent_population, amount, probabilities = None):
         """
-        returns randomly selected individuals
+        returns randomly selected individuals 
         """
         sorted_population = sorted(parent_population)
         if probabilities is None:
             total_proportion = len(sorted_population)
             probabilities = []
             for i in range(total_proportion):
-                probability = total_proportion/(total_proportion - i)
+                probability = (total_proportion-i)/total_proportion
                 probabilities.append(probability)
         sample = rd.choices(sorted_population, weights = probabilities, k = amount)
         return sample
@@ -125,7 +159,7 @@ class GeneticProgram:
     def _tournament_selection(self, parent_population, amount):
         selection = []
         for i in range(amount):
-            competitors = rd.choices(parent_population, amount = self.tournament_size)
+            competitors = rd.choices(parent_population, k = self.tournament_size)
             winner = sorted(competitors)[0]
             selection.append(winner)               
         return selection
---
+++ TreeFunction.py
@@ -0,0 +1,271 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 28 10:52:52 2019
+
+@author: 33fred33 and Kevin Galligan
+"""
+import operator
+from inspect import signature
+import random as rd
+import math
+
+def safe_divide(a, b):
+    """
+    Arguments:
+        a is a number
+        b is a number
+    Executes a/b. If b=0, returns a
+    """
+    if b == 0 :
+        return a
+    else:
+        return a/b
+
+class TreeFunction:
+    def __init__(self,
+            features,
+            evaluate_fitness_f,
+            max_initial_depth = 3,
+            max_depth = 15,
+            initialisation_method = "ramped half half",
+            mutation_method = "subtree"):
+        """
+        Positional arguments:
+            features: number of features the tree will expect
+            evaluate_fitness_f: function to be called when evaluation is needed
+                Positional arguments:
+                    population: population to be evaluated.
+                Returns the list of evaluations
+        Keyword arguments:
+            max_initial_depth: restricts depth of generated functions in their tree representation. Default is 3
+            max_depth: stablishes the limit for the tree depth, to be tested after every mutation or crossover. Default is 15
+            initialisation_method: can be "ramped half half", "grow", "full", "ramped full", "ramped grow", "half half". Default is ramped half half
+            mutation_method: can be "subtree" or "unit". Default is subtree
+        """
+        self.features = features
+        self.evaluate = evaluate_fitness_f
+        self.max_initial_depth = max_initial_depth
+        self.max_depth = max_depth
+        self.initialisation_method = initialisation_method
+        self.mutation_method = mutation_method
+    
+    def _generate_terminal(self):
+        """
+        Generates a number that can be an index for the features vector, or a random number between -1 and 1
+        """
+        value = rd.randint(0, self.features)
+        if value == self.features:
+            value = np.random.uniform(low=-1, high=1)
+        return value
+
+    def _generate_operator(self):
+        """
+        Generates an operator.
+        Operators can be add, sub, mul or safe divide
+        """
+        return rd.choice([
+            operator.add,
+            operator.sub,
+            operator.mul,
+            safe_divide])
+        
+    def _generate_individual_full(self, max_depth, parent=None, depth=0):
+        """
+        Generates a random individual using Koza's full method
+        """
+        if depth == max_depth:
+            terminal = self._generate_terminal()
+            return Node(terminal, parent = parent)
+        else:
+            operator = self._generate_operator()
+            sig = signature(operator)
+            arity = len(sig.parameters)
+            node = Node(operator, parent = parent)
+            for _ in range(arity):
+                node.children.append(self._generate_individual_full(max_depth, parent=node, depth=depth+1))
+            return node
+
+    def _generate_individual_grow(self, max_depth, parent=None, depth=0):
+        """
+        Generates a random individual using Koza's grow method
+        """
+        if depth == max_depth:
+            terminal = self._generate_terminal()
+            return Node(terminal, parent = parent)
+        else:
+            if rd.choice([True, False]):
+                operator = self._generate_operator()
+                sig = signature(operator)
+                arity = len(sig.parameters)
+                node = Node(operator, parent = parent)
+                for _ in range(arity):
+                    node.children.append(self._generate_individual_full(max_depth, parent=node, depth=depth+1))
+                return node
+            else:
+                terminal = self._generate_terminal()
+                return Node(terminal, parent = parent)
+        
+    def generate_population(self, size):
+        """
+        Positional arguments:
+            size: is an integer
+        Generates a population of size: size
+        """
+        if self.initialisation_method == "half half":
+            first_half = math.ceil(size/2)
+            population = [self._generate_individual_grow(self.max_initial_depth) for _ in range(first_half)]
+            second_half = size - first_half
+            population.extend([self._generate_individual_full(self.max_initial_depth) for _ in range(second_half)])
+
+        elif self.initialisation_method == "grow":
+            population = [self._generate_individual_grow(self.max_initial_depth) for _ in range(size)]
+
+        elif self.initialisation_method == "full":
+            population = [self._generate_individual_full(self.max_initial_depth) for _ in range(size)]
+
+        elif self.initialisation_method == "ramped half half":
+            parts = self.max_initial_depth - 1
+            part_size = int(size / parts)
+            remainder_size = size % parts
+            population = [self._generate_individual_full(self.max_initial_depth) for _ in range(remainder_size)]
+            for max_depth in range(2, self.max_initial_depth+1):
+                first_half = math.ceil(part_size/2)
+                population.extend([self._generate_individual_grow(max_depth) for _ in range(first_half)])
+                second_half = part_size - first_half
+                population.extend([self._generate_individual_full(max_depth) for _ in range(second_half)])
+
+        elif self.initialisation_method == "ramped grow":
+            parts = self.max_initial_depth - 1
+            part_size = int(size / parts)
+            remainder_size = size % parts
+            population = [self._generate_individual_grow(self.max_initial_depth) for _ in range(remainder_size)]
+            for max_depth in range(2, self.max_initial_depth+1):
+                population = [self._generate_individual_grow(max_depth) for _ in range(part_size)]
+
+        elif self.initialisation_method == "full":
+            parts = self.max_initial_depth - 1
+            part_size = int(size / parts)
+            remainder_size = size % parts
+            population = [self._generate_individual_full(self.max_initial_depth) for _ in range(remainder_size)]
+            for max_depth in range(2, self.max_initial_depth+1):
+                population = [self._generate_individual_full(max_depth) for _ in range(part_size)]
+
+        else:
+            print("No correct initialisation method was given")
+        return population
+    
+    def _collect_nodes(self, root_node):
+        """
+        Positional arguments:
+            root_node: node
+        Returns a list with all the nodes existing in the tree with roo_node as the root node.
+        """
+        nodes = [root_node]
+        i = 0
+        while i < len(nodes):
+            if not nodes[i].is_terminal():
+                nodes.extend(nodes[i].children)
+            i += 1
+        return nodes
+    
+    def mutate(self, parent):
+        """
+        Positional arguments:
+            parent: node
+        Returns the same tree with a mutation applied
+        """
+        new_individual = parent.copy()
+        nodes = self._collect_nodes(new_individual)
+        if self.mutation_method == "unit":
+            mutation_point = rd.choice(nodes)
+            if mutation_point.is_terminal():
+                mutation_point.content = self._generate_terminal()
+            else:
+                mutation_point.content = self._generate_operator() #MISSING: verify arity
+        elif self.mutation_method == "subtree":
+            node_to_overwrite = rd.choice(nodes)
+            subtree = self._generate_individual_grow(self.max_initial_depth, parent = node_to_overwrite.parent)
+            if node_to_overwrite.is_root():
+                return subtree
+            else:
+                parent = node_to_overwrite.parent
+                for i,child in enumerate(parent.children):
+                    if child == node_to_overwrite:
+                        parent.children[i] = subtree
+                        break
+
+        return new_individual
+        
+    def crossover(self, first_parent, second_parent):
+        """
+        Positional arguments:
+            first_parent: node
+            second_parent: node
+        Returns the offspring tree resulting from the crossover between the first_parent and the second_parent
+        """
+        second_nodes = self._collect_nodes(second_parent)
+        crossover_section = rd.choice(second_nodes).copy()
+        new_individual = first_parent.copy()
+        node_to_overwrite = rd.choice(self._collect_nodes(new_individual)[:1]) #ignores the root node
+        if node_to_overwrite.is_root():
+            return crossover_section
+        else:
+            parent = node_to_overwrite.parent
+            for i,child in enumerate(parent.children):
+                if child == node_to_overwrite:
+                    parent.children[i] = crossover_section
+                    break
+            return new_individual
+    
+class Node:
+    def __init__(self, content, *children, parent = None,):
+        """
+        Positional arguments:
+            content: can be an operator or a number
+            childs: expects any number of nodes from this same class
+        Keyword arguments:
+            parent: expects a node from this same class
+                default: None
+        """
+        self.content = content
+        self.parent = parent
+        self.children = []
+        for child in children:
+            self.children.append(child)
+        
+    def __str__(self):
+        if self.is_terminal():
+            return str(self.content)
+        else:
+            name_string = "(" + self.content.__name__
+            for child in self.children:
+                name_string += " " + str(child)
+            name_string += ")"
+            return name_string
+    
+    def is_terminal(self):
+        return self.child == []
+    
+    def is_root(self):
+        return self.parent is None
+
+    def my_max_depth(self, node, depth = 0, depths=[0]):
+        new_depth = depth + 1
+        depths.append(new_depth)
+        if self.is_terminal:
+            return(max(depths))
+        else:
+            for child in children:
+                self.my_max_depth(child, new_depth, depths)
+    
+    def copy(self, parent=None):        
+        the_copy = Node(self.content, parent = self.parent)
+        if not self.is_terminal():
+            for child in self.children:
+               the_copy.children.append(child.copy(parent = the_copy))
+        return the_copy
+
+
+
+
